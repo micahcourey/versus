8f5c6de493aca8a06bf57d02ad89b62ed22b40cc
micahcourey <micahjcourey@gmail.com>
2015-12-24 12:19:45 -0800

    Add next test

--- versus-server/test/core_spec.js
+++ versus-server/test/core_spec.js
@@ -1,7 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
@@ -16,6 +15,22 @@ describe('application logic', () => {
       }));
     });
 
+  });
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote',() => {
+      const state = Map({
+        entries: List.of('coffee', 'tea', 'juice')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('coffee', 'tea')
+        }),
+        entries: List.of('juice')
+      }));
+    });
   });
 
 });
