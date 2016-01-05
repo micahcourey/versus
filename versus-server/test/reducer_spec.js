39e7929b5a5417629e4500fbf141e267862972d2
micahcourey <micahjcourey@gmail.com>
2016-01-04 17:26:35 -0800

    Add tests to check NEXT and VOTE to make sure they are properly delegated by the reducer

--- versus-server/test/reducer_spec.js
+++ versus-server/test/reducer_spec.js
@@ -14,5 +14,36 @@ describe('reducer', () => {
       entries: ['coffee']
     }));
   });
-  
-})
+
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['coffee', 'tea']
+    });
+    const action = {type: 'NEXT'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      entries: ['coffee']
+    }));
+  });
+
+  it('handles VOTE', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['coffee', 'tea']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'coffee'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['coffee', 'tea'],
+        tally: {coffee: 1}
+      },
+      entries: []
+    }));
+  });
+
+});
