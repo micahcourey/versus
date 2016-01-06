a476ac59fac1db200def41ce84ec5f434c0bc835
micahcourey <micahjcourey@gmail.com>
2016-01-06 13:21:45 -0800

    Add reducer function to return the current state

--- versus-server/src/reducer.js
+++ versus-server/src/reducer.js
@@ -1,3 +1,15 @@
-export default function reducer(state, action) {
-  
+import {setEntries, next, restart, vote, INITIAL_STATE} from './core';
+
+export default function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case 'SET_ENTRIES':
+      return setEntries(state, action.entries);
+    case 'NEXT':
+      return next(state);
+    case 'RESTART':
+      return restart(state);
+    case 'VOTE':
+      return state.update('vote', voteState => vote(voteState, action.entry, action.clientID));
+  }
+  return state;
 }
